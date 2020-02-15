49ebf9cfdc9dd381eaa67dbfc110cdb21df1e661
Michal <michal.malczyk@gmail.com>
2020-02-15 16:47:25 +0100

    init repo

---
+++ src/main/java/com/example/javacoursesapi/JavaCoursesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacoursesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoursesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacoursesapi/JavaCoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacoursesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
